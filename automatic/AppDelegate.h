93b886f868bd4acbc055993b56ba2b67d2361634
shiki <1002409455@qq.com>
2017-06-27 14:14:21 +0800

    Initial Commit

---
+++ automatic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  automatic
+//
+//  Created by shiki on 2017/6/27.
+//  Copyright © 2017年 两仪式. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ automatic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  automatic
+//
+//  Created by shiki on 2017/6/27.
+//  Copyright © 2017年 两仪式. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
