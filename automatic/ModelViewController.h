067f8d8a1ef2d4fc7839375635ab0d0d653b744e
shiki <1002409455@qq.com>
2017-07-07 15:38:10 +0800

    完成剩余功能

---
+++ automatic/ModelViewController.h
@@ -0,0 +1,13 @@
+//
+//  ModelViewController.h
+//  automatic
+//
+//  Created by shiki on 2017/7/7.
+//  Copyright © 2017年 两仪式. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModelViewController : UIViewController
+
+@end
